599811ee3bd0c4e8be8d94b30a8881cac99eb813
sirasatarato <sirasatarato@gmail.com>
2020-09-17 15:15:29 +0900

    Reform MVVMDataLive

--- MVVMDataLive/app/src/main/java/com/silso/mvvmdatalive/MainActivity.kt
+++ MVVMDataLive/app/src/main/java/com/silso/mvvmdatalive/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    
         db = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         // Get ViewModel.
